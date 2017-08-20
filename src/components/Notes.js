3f9563e9df4ae1e5080648a3cd9c5021c5613d93
Rob Burgess <rob.burgess@live.co.uk>
2017-08-20 21:13:54 +0100

    Added saving of notes

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Subheader from 'material-ui/Subheader';
 
-export default function Notes() {
-  return (
-    <div style={{width: '700px', float: 'left'}}>
-      <Subheader>Notes</Subheader>
-      <div contentEditable style={{height: '600px', padding: '20px'}}></div>
-    </div>
-  );
+class Notes extends Component {
+  handleBeforeUnload = () => {
+    localStorage.setItem('pad-notes', this.notesHolder.innerHTML);
+  }
+
+  componentDidMount() {
+    window.addEventListener('beforeunload', this.handleBeforeUnload);
+    this.notesHolder.innerHTML = localStorage.getItem('pad-notes') || '';
+  }
+  
+  componentWillUnMount() {
+    window.addEventListener('beforeunload', this.handleBeforeUnload);
+  }
+
+  render(){
+    return (
+      <div style={{width: '700px', float: 'left'}}>
+        <Subheader>Notes</Subheader>
+        <div contentEditable style={{height: '600px', padding: '20px'}} ref={x => this.notesHolder = x}></div>
+      </div>
+    );
+  }
 }
+
+export default Notes;
